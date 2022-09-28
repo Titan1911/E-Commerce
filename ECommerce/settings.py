38db840572adb6f19dc8c21dff599618d12a69b4
Titan1911 <sahilahuja1911@gmail.com>
2022-09-29 01:34:23 +0530

    requirements.txt changes

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -165,7 +165,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUD_NAME'),
-    'API_KEY': env('COULD_API_KEY'),
+    'API_KEY': env('CLOUD_API_KEY'),
     'API_SECRET': env('CLOUD_API_SECRET')
 }
 
